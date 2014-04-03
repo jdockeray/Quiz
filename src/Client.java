9c84f31619fd65901e95f3c5beab4a50b16db835
James <jdockeray>
2014-04-03 22:33:43 +0100

    removed source files as have a clearer idea about how to structure an RMI server client model

--- src/Client.java
+++
@@ -1,9 +0,0 @@
-public interface Client extends SetUpClient, PlayerClient {
-	/**
-	 * @param playGameTask
-	 * Starts a new quiz game with the Server
-	 */
-	void playGame(Runnable playGameTask);
-	
-	
-}
--- src/SetUpClient.java
+++
@@ -1,7 +0,0 @@
-public interface SetUpClient{
-	/**
-	 * This interface connects to the Quiz Game Server and manages the set up of a new quiz. Once finished it 
-	 * invokes the player client
-	 * 
-	 */
-}
