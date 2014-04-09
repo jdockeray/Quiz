418bdd33767593854cda6a7649812383a5d2868c
jdockeray <jdockeray20@gmail.com>
2014-04-09 22:25:50 +0100

    commented out security manager so that it works on Eclipse

--- src/quiz/SetUpClient.java
+++ src/quiz/SetUpClient.java
@@ -5,20 +5,16 @@ import java.rmi.Naming;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
 import compute.Compute;
 
 public class SetUpClient {
 	
-
-
     public static void main(String args[]) {
     	QuizBuilder quizBuilder=new QuizBuilder();
     	List<String[]> myQuiz=quizBuilder.questionsBuilder();
-    	
-        if (System.getSecurityManager() == null) {
-            System.setSecurityManager(new SecurityManager());
-        }
+    //	if (System.getSecurityManager() == null) {
+      //      System.setSecurityManager(new SecurityManager());
+        //}
         try {              
             Compute comp = (Compute) Naming.lookup("//127.0.0.1:1099/Compute"); 
             int id = comp.getId();
