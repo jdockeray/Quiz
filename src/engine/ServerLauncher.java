9f9eed2f7d975634e30c79079286f3d211d6d106
jdockeray <jdockeray20@gmail.com>
2014-04-09 22:24:20 +0100

    disabled security files, so that it runs on eclipse with the tests

--- src/engine/ServerLauncher.java
+++ src/engine/ServerLauncher.java
@@ -8,17 +8,18 @@ import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 
-
 import compute.Compute;
+
+
 
 
 public class ServerLauncher {
 	public static void main(String[] args){
-			if(System.getSecurityManager() == null) {
-				System.setSecurityManager(new RMISecurityManager());
-			}
+			//if(System.getSecurityManager() == null) {
+				//System.setSecurityManager(new RMISecurityManager());
+			//}
 			try {
-				LocateRegistry.createRegistry(1099);
+				//LocateRegistry.createRegistry(1099);
 			    Compute engine = new ComputeEngine();
 				String registryHost = "//localhost/";
 				String serviceName = "Compute";
