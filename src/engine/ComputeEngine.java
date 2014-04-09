b0c1bb026d42afc55b47590e922d37b66fe6da85
James <jdockeray>
2014-04-09 14:18:32 +0100

    added add Question and return id methods

--- src/engine/ComputeEngine.java
+++ src/engine/ComputeEngine.java
@@ -2,11 +2,13 @@ package engine;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
+import java.util.Random;
 import compute.Compute;
-import compute.Task;
+
 
 public class ComputeEngine extends UnicastRemoteObject implements Compute {
 
@@ -14,20 +16,35 @@ public class ComputeEngine extends UnicastRemoteObject implements Compute {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	public Map<Integer, String[]> Questions = new HashMap<Integer, String[]>();
+	public Map<Integer, List<Question>> Questions = new HashMap<Integer, List<Question>>();
 	
 	public ComputeEngine() throws RemoteException {
         super();
     }
 
-    public <T> T executeTask(Task<T> t) {
-        return t.execute();
+    
+    public String addQuestion (String question, String answer, int id)throws RemoteException{
+    		Question theQuestion = new BooleanQuestion(question, Integer.parseInt(answer)==1?true:false);
+    		List<Question> QuestionArray=Questions.get(id);
+    		if(QuestionArray==null){
+    			QuestionArray=new ArrayList<Question>();
+    		}
+    		QuestionArray.add(theQuestion);
+    		return "";
     }
     
-    public String[] addQuestion (String str, String str2)throws RemoteException{
-    	System.out.println("added to db");
-    	String[] strArray = {str, str2};
-    	return Questions.put(001, strArray);
+    public int getId(){
+    	Random r = new Random();
+		int x = r.nextInt(99999999);
+    	try{
+    		if(Questions.containsKey(x)){
+    			throw new IllegalArgumentException();
+    		};
+    	}
+    	catch(IllegalArgumentException ex){
+    		getId();
+    	}
+    	return x;
     }
 
 
