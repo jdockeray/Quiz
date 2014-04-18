1d296065373ccc8b2f8800d861bd21d6ede0d996
jdockeray <jdockeray20@gmail.com>
2014-04-18 20:49:53 +0100

    refactored

--- src/compute/Compute.java
+++ src/compute/Compute.java
@@ -2,8 +2,11 @@ package compute;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
+import java.util.Map;
 
 import server.quiz.ClientSetUp;
+import server.quiz.Question;
 
 
 public interface Compute extends Remote {
@@ -17,10 +20,46 @@ public interface Compute extends Remote {
      * @param question
      * @param answer
      * @param fakeAnswers
-     * @throws RemoteException 
+     * @throws RemoteException, 
+     * @throws  Illegal Argument Exception if any of the arguments is null
      */
-    void addMultiChoiceQuestion(int id, String question,  String answer, String... fakeAnswers) throws RemoteException;
+    void addMultiChoiceQuestion(int id, String question,  String answer, String... fakeAnswers) throws RemoteException, IllegalArgumentException;
     
+    
+    /**
+     * Use this when adding first Question to db, it add the first question as well as creates an id for this quiz
+     * 
+     * @param question
+     * @param answer
+     * @param fakeAnswers
+     * @return The id for the quiz
+     * @throws RemoteException
+     * @throws IllegalArgumentException
+     */
+    int addFirstMultiChoiceQuestion(String question,  String answer, String... fakeAnswers) throws RemoteException, IllegalArgumentException;
+
+    /**
+     * Returns the a SetUp Object that contains methods for creating new quizes
+     * 
+     * @return a ClientSetUp Object
+     * @throws RemoteException
+     */
     ClientSetUp setUpClientObject() throws RemoteException;
+    
+    
+    /**
+     * @return The Map of all Quizes
+     * @throws RemoteException
+     */
+    public Map<Integer, List<Question>> getQuizArray() throws RemoteException;
+    
+    /**
+     * Returns the Quiz for the set id
+     * 
+     * @param id
+     * @return
+     * @throws RemoteException
+     */
+    public List<Question> getQuiz(int id) throws RemoteException;
 }
 
